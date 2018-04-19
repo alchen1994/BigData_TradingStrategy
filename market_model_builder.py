8ac729f56c70931e7ec2336d46d0b659af3d3516
alchen1994 <31779347+alchen1994@users.noreply.github.com>
2018-04-19 18:05:50 -0400

    Add files via upload

--- market_model_builder.py
+++ market_model_builder.py
@@ -1,23 +1,45 @@
-from keras.models import Sequential
-from keras.layers import Dense
+# from keras.models import Sequential
+from keras.models import Model
+from keras.layers import Dense, Input, Add, merge, Conv2D,Flatten
 from keras.optimizers import Adam
+from keras.layers.merge import concatenate
+from keras.layers.advanced_activations import LeakyReLU
+
 
 class MarketDeepQLearningModelBuilder():
    
-    def __init__(self, state_size = 9, action_size = 3):
-        self.state_size = state_size
+    def __init__(self, action_size = 3):
         self.action_size = action_size
         self.learning_rate = 0.001
 
     def buildModel(self):
-        model = Sequential()
-        model.add(Dense(48, input_shape = (self.state_size,), activation='relu'))
-        model.add(Dense(96, activation='relu'))
-        # model.add(Dense(128, activation='relu'))
-        # model.add(Dense(96, activation='relu'))
-        model.add(Dense(96, activation='relu'))
-        model.add(Dense(48, activation='relu'))
-        model.add(Dense(self.action_size, activation='linear'))
+
+        input1 = Input(shape=(2,))
+        input2 = Input(shape=(5,10,1))
+
+        pre = []
+
+        net1 = Dense(8, activation='relu')(input1)
+
+        net2 = Conv2D(filters=1024, kernel_size=(1, 10), strides=1, data_format='channels_last')(input2)
+        net2 = LeakyReLU(alpha = 0.001)(net2)
+        net2 = Flatten()(net2)
+        net2 = Dense(48)(net2)
+        net2 = LeakyReLU(alpha = 0.001)(net2)
+        
+        pre.append(net1)
+        pre.append(net2)
+
+        hidden1 = concatenate(pre, axis=1)
+        hidden2 = Dense(96, activation='relu')(hidden1)
+        hidden3 = Dense(128, activation='relu')(hidden2)
+        hidden4 = Dense(128, activation='relu')(hidden3)
+        hidden5 = Dense(96, activation='relu')(hidden4)
+        output_main = Dense(self.action_size, activation='linear')(hidden5)
+
+
+        model = Model(inputs = [input1,input2], outputs = output_main)
+
 
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
 
@@ -27,10 +49,6 @@ class MarketDeepQLearningModelBuilder():
 
 
 
-# from keras.models import Model
-# from keras.layers import merge, Conv2D, MaxPooling2D, Input, Dense, Flatten, Dropout, Reshape, TimeDistributed, BatchNormalization, Merge, merge
-# from keras.layers.merge import concatenate
-# from keras.layers.advanced_activations import LeakyReLU
     # def buildModel(self):
 
 
